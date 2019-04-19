a954e3d062d573390d011b45e525ca08aa7b91ef
Laura <laura.robertson64@gmail.com>
2019-04-19 14:52:39 -0500

    skeleton

--- removeNthNode/removeNthNode.js
+++ removeNthNode/removeNthNode.js
@@ -12,3 +12,19 @@
 // Follow up:
 
 // Could you do this in one pass?
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+    
+};
