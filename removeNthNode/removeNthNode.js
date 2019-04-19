ac987058c9bc16e9f5e8390a7cede0461d5933b6
Laura <laura.robertson64@gmail.com>
2019-04-19 15:42:51 -0500

    first pass at removeNthNode

--- removeNthNode/removeNthNode.js
+++ removeNthNode/removeNthNode.js
@@ -26,5 +26,41 @@
  * @return {ListNode}
  */
 var removeNthFromEnd = function(head, n) {
-    
+  //iterate over linked list
+  //make a prev, curr, and next variables
+  //while !found (change to true when .next n times found)
+    //call .next n times on curr
+      //when n x .next is null
+      //remove that node
+      //set prev.next to next
+      //set curr.next to null
+      //mark found as true
+  //each time we check .next n times
+  //afterward, if we find it is nothing, change prev to curr, curr to next, and next to curr.next
+  
+  //once we remove n, we should break the loop and return head (which should remain unchanged)
+  
+  let prev = null;
+  let curr = head;
+  let next = head.next;
+  let found = false;
+  while (!found) {
+      let i = 0;
+      let nthNode = curr;
+      while (i < n) {
+          if (nthNode.next === null) {
+              found = true;
+              prev.next = next;
+              curr.next = null;
+          }
+          i++;
+          nthNode = curr.next;
+      }
+      if (!found) {
+          prev = curr;
+          curr = next;
+          next = curr.next;
+      }
+  }
+  return head;
 };
