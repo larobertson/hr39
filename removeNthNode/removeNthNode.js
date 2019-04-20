7fd0a3c9a3ada0e0e363ee53a712ef88cf56ef37
Laura <laura.robertson64@gmail.com>
2019-04-20 11:40:19 -0500

    solved removeNthNode in more concise manner

--- removeNthNode/removeNthNode.js
+++ removeNthNode/removeNthNode.js
@@ -13,59 +13,79 @@
 
 // Could you do this in one pass?
 
+
+ //Definition for singly-linked list.
+ function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
 /**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
- */
-/**
- * @param {ListNode} head
- * @param {number} n
- * @return {ListNode}
- */
+* @param {ListNode} head
+* @param {number} n
+* @return {ListNode}
+*/
+
 var removeNthFromEnd = function(head, n) {
-  //iterate over linked list
-  //make a prev, curr, and next variables
-  //while !found (change to true when .next n times found)
-    //call .next n times on curr
-      //when n x .next is null
-      //remove that node
-      //set prev.next to next
-      //set curr.next to null
-      //mark found as true
-  //each time we check .next n times
-  //afterward, if we find it is nothing, change prev to curr, curr to next, and next to curr.next
-  
-  //once we remove n, we should break the loop and return head (which should remain unchanged)
-  
-  let prev = null;
-  let curr = head;
-  let next = head.next;
-  let found = false;
-  while (!found) {
-      let i = 0;
-      let nthNode = curr;
-      while (i < n) {
-          if (nthNode.next === null) {
-              found = true;
-              if (curr === head) {
-                  head = head.next;
-              }
-              if (prev) {
-                  prev.next = next;
-              }
-              curr.next = null;
-          }
-          i++;
-          nthNode = nthNode.next;
-      }
-      if (!found) {
-          prev = curr;
-          curr = next;
-          next = curr.next;
-      }
-  }
-  return head;
+let follower = head;
+let iterator = head;
+
+while (iterator) {
+iterator = iterator.next;
+if (n-- < 0) {
+  follower = follower.next;
+}
+}
+
+if (n === 0) return follower.next;
+follower.next = follower.next.next;
+return head;
 };
+
+//Naive solution, fast, but takes up memory (space)
+// var removeNthFromEnd = function(head, n) {
+//     //iterate over linked list
+//     //make a prev, curr, and next variables
+//     //while !found (change to true when .next n times found)
+//       //call .next n times on curr
+//         //when n x .next is null
+//         //remove that node
+//         //set prev.next to next
+//         //set curr.next to null
+//         //mark found as true
+//     //each time we check .next n times
+//     //afterward, if we find it is nothing, change prev to curr, curr to next, and next to curr.next
+
+//     //once we remove n, we should break the loop and return head (which should remain unchanged)
+
+//     let prev = null;
+//     let curr = head;
+//     let next = head.next;
+//     let found = false;
+//     while (!found) {
+//         let i = 0;
+//         let nthNode = curr;
+//         while (i < n) {
+//             if (nthNode.next === null) {
+//                 found = true;
+//                 if (curr === head) { //the tricky part: if the removed element is head then we must reassign head
+//                     head = head.next;
+//                 }
+//                 if (prev) {
+//                     prev.next = next;
+//                 }
+//                 curr.next = null;
+//             }
+//             i++;
+//             nthNode = nthNode.next;
+//         }
+//         if (!found) {
+//             prev = curr;
+//             curr = next;
+//             next = curr.next;
+//         }
+//     }
+//     return head;
+// };
+
+//O(n)
