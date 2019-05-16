7cf73c7796bf9d18a1506cc90cac6a2a67ec54ea
Laura <laura.robertson64@gmail.com>
2019-05-16 17:28:24 -0500

    passes two more tests

--- hackerrank/problem-solving/minimumSwaps/minimumSwaps.js
+++ hackerrank/problem-solving/minimumSwaps/minimumSwaps.js
@@ -3,9 +3,9 @@ module.exports = function (arr) {
   //find the next number in sequence(wherever it is) and move to it's position
 
   let swap = 0;
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length - 1; i++) {
     if (arr[i] !== i + 1) {
-      for (let k = 0; k < arr.length; k++) {
+      for (let k = i; k < arr.length; k++) {
         if (arr[k] === i + 1) {
           [arr[i], arr[k]] = [arr[k], arr[i]]
           swap++;
