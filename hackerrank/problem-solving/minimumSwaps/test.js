db9a4b930ad5cdff74c1e7aee7454cfc2ed63633
Laura <laura.robertson64@gmail.com>
2019-05-16 17:07:01 -0500

    solved minimum swaps

--- hackerrank/problem-solving/minimumSwaps/test.js
+++ hackerrank/problem-solving/minimumSwaps/test.js
@@ -17,4 +17,13 @@ describe('minimumSwaps', function() {
   it('test 4', function() {
     expect(minimumSwaps([1, 3, 5, 2, 4, 6, 7])).to.equal(3)
   });
+  it('test 5', function() {
+    expect(minimumSwaps([1, 3, 5, 2, 4, 6, 7])).to.equal(3)
+  });
+  it('test 6', function() {
+    expect(minimumSwaps([2, 3, 4, 1, 5])).to.equal(3)
+  });
+  it('test 7', function() {
+    expect(minimumSwaps([4, 3, 1, 2])).to.equal(3)
+  });
 })
