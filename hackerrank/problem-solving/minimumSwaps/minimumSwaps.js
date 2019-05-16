956a8cab06032df40a8b211857b11544143c4ac7
Laura <laura.robertson64@gmail.com>
2019-05-16 17:02:34 -0500

    added test functionality to minimum swaps

--- hackerrank/problem-solving/minimumSwaps/minimumSwaps.js
+++ hackerrank/problem-solving/minimumSwaps/minimumSwaps.js
@@ -1,3 +1,17 @@
 module.exports = function (arr) {
-  //
+  //iterate over the array
+  //find the next number in sequence(wherever it is) and move to it's position
+
+  let swap = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== i + 1) {
+      for (let k = 0; k < arr.length; k++) {
+        if (arr[k] === i + 1) {
+          [arr[i], arr[k]] = [arr[k], arr[i]]
+          swap++;
+        }
+      }
+    }
+  }
+  return swap;
 }
--- hackerrank/problem-solving/minimumSwaps/test.js
+++ hackerrank/problem-solving/minimumSwaps/test.js
@@ -4,5 +4,17 @@ const minimumSwaps = require('./minimumSwaps')
 describe('minimumSwaps', function() {
   it('test 0', function() {
     expect(minimumSwaps([2,1,3])).to.equal(1)
-  })
+  });
+  it('test 1', function() {
+    expect(minimumSwaps([7, 1, 3, 2, 4, 5, 6])).to.equal(5)
+  });
+  it('test 2', function() {
+    expect(minimumSwaps([4, 3, 1, 2])).to.equal(3)
+  });
+  it('test 3', function() {
+    expect(minimumSwaps([2, 3, 4, 1, 5])).to.equal(3)
+  });
+  it('test 4', function() {
+    expect(minimumSwaps([1, 3, 5, 2, 4, 6, 7])).to.equal(3)
+  });
 })
